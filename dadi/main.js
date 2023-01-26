5d5647d85ab8f2324b9c523d5b1742ea42bf45ba
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-01-26 15:50:24 +0100

    entering everything

--- dadi/main.js
+++ dadi/main.js
@@ -3,7 +3,25 @@
 
 // GENERARE UN NUMERO RANDOM DA 1 A 6 PER IL GIOCATORE
 let numeroGiocatore = Math.floor(Math.random() * 6) + 1;
+alert("Numero del giocatore: " + numeroGiocatore);
 console.log("Numero del giocatore: " + numeroGiocatore);
 
 let numeroComputer = Math.floor(Math.random() * 6) + 1;
+alert("Numero del computer: " + numeroComputer);
 console.log("Numero del computer: " + numeroComputer);
+
+// ISTRUZIONI CONDIZIONALI
+if (numeroGiocatore > numeroComputer) {
+    alert("Congratulazioni, hai vinto!");
+    console.log("Congratulazioni, hai vinto!");
+}
+
+else if (numeroGiocatore < numeroComputer) {
+    alert("Mi dispiace, hai perso");
+    console.log("Mi dispiace, hai perso");
+}
+
+else {
+    alert("È un pareggio, aggiorna la pagina per rigiocare!");
+    console.log("È un pareggio, aggiorna la pagina per rigiocare!");
+}
